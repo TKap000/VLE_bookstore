ec1e3ebbab5b98f1c42ade55f474ffa826f72796
TKap000 <kasparkoolis@gmail.com>
2023-10-22 23:26:38 +0300

    landing page viimane version

---
+++ script.js
@@ -0,0 +1,16 @@
+
+// Karuselli aluseks on kasutatud https://www.w3schools.com/howto/howto_js_slideshow.asp näidet.
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}
+    slides[slideIndex-1].style.display = "block";
+    setTimeout(showSlides, 5000);
+  }
